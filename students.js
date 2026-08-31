3d4433749b1c8ebd6c528289934c06871e0eab38
Matthew-Bueno <matthew.bueno09@gmail.com>
2026-08-31 07:41:24 -0700

    trying to make article in JS

---
+++ students.js
@@ -0,0 +1,7 @@
+let article = document.createElement("article");
+
+article.classList.add("discussion-post", "version-one");
+
+let sec = document.getElementById("discussion-board");
+
+sec.appendChild("article");
